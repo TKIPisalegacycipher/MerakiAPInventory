56067e79214774bd7ad068aeb1a1871dd4ab9d62
John M. Kuchta <git@chir.us>
2020-04-21 00:08:11 -0700

    Reworked the app to use the Meraki SDK instead of invoking the API directly, which makes the app 'safer' and more readable. It also will do more work for you! Instead of having to enter an API key and a network and a network name, you can just enter the API key and the app will show you all your networks.

--- app.py
+++ app.py
@@ -1,113 +1,150 @@
 from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-import requests
-import json
+
+# We're only importing this for its ability to convert JSON to XLSX.
 import pandas
+
+from meraki_sdk.meraki_sdk_client import MerakiSdkClient
+import jsonpickle
+
+
+# With the Meraki SDK, we don't need these.
+#import requests
+#import json
 
 app = Flask(__name__) 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
+class Api(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	key = db.Column(db.String(40))
+	name = db.Column(db.String(100))
+	lastfour = db.Column(db.String(4))
+	orgs = db.relationship('OrganizationList', backref='api_key')
 
 class OrganizationList(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(200), nullable=False)
-	apiKey = db.Column(db.String(40))
-
-	def __repr__(self):
-		return '<Organization %r>' % self.id
+	name = db.Column(db.String(200))
+	key_id = db.Column(db.Integer, db.ForeignKey('api.id'))
 
 # Global variables; these are idential to the variables you might use in Postman for a given collection
-baseUrl = "https://api.meraki.com/api/v0"
-perPage = 1000
 outputFilePath = "./static/excel/"
 
-# View Organizations
+# You don't need these with the Meraki SDK.
+# baseUrl = "https://api.meraki.com/api/v0"
+# perPage = 1000
+
+# View API keys
 @app.route('/', methods=['POST','GET'])
 def index():
 	if request.method == 'POST':
-		organizationId = request.form['id']
-		organizationName = request.form['name']
-		organizationApiKey = request.form['apiKey']
-		
-		new_organization = OrganizationList(
-			id = organizationId, 
-			name = organizationName, 
-			apiKey = organizationApiKey
-			)
+		key = request.form['apiKey']
+		name = request.form['name']
+		lastfour = key[len(key)-4:len(key)]
 
+		new_key = Api(
+			key = key,
+			name = name,
+			lastfour = lastfour
+			)
+		
 		try:
-			db.session.add(new_organization)
+			db.session.add(new_key)
 			db.session.commit()
 			return redirect('/')
+
 		except:
-			return "There was an error adding your organization."
+			return "There was an error adding your API key."
+		
 	else:
 		try:
-			organizations = OrganizationList.query.order_by(OrganizationList.name).all()
-			return render_template('index.html', organizations=organizations)
+			api_keys = Api.query.order_by(Api.key).all()
+			return render_template('index.html', keys=api_keys)
 		except:
 			return render_template('index.html')
 
-# Delete Organization
-@app.route('/<int:id>/delete')
+
+# Delete Key
+@app.route('/key/<int:id>/delete')
 def delete(id):
-	organization_to_delete = OrganizationList.query.get_or_404(id)
+	key_to_delete = Api.query.get_or_404(id)
 
 	try:
-		db.session.delete(organization_to_delete)
+		db.session.delete(key_to_delete)
 		db.session.commit()
 		return redirect('/')
 	except:
-		return 'Problem deleting organization.'
+		return 'Problem deleting key.'
+
+# View Organizations
+@app.route('/key/<int:id>/organizations')
+def organizations(id):
+
+	# Connect to the Meraki API.
+	x_cisco_meraki_api_key = Api.query.get_or_404(id).key
+	client = MerakiSdkClient(x_cisco_meraki_api_key)
+
+	# get all organizations
+	orgs = client.organizations.get_organizations()
+
+	try:
+		return render_template('organizations.html', organizations = orgs, id = id)
+	except:
+		return render_template('organizations.html')
 
 # View Inventory
-@app.route('/<int:id>/inventory')
-def inventory(id):
-	organization = OrganizationList.query.get_or_404(id)
-	apiQueryHeaders = {
-		'X-Cisco-Meraki-API-Key': organization.apiKey,
-		'Accept': '*/*',
-		'Accept-Encoding': 'gzip, deflate, br',
-		'Connection': 'keep-alive'
-	}
+@app.route('/key/<int:id>/organization/<organization_id>/inventory')
+def inventory(id, organization_id):
 
-	apiQueryURI = baseUrl + "/organizations/" + str(id) + "/inventory"
-	
-	apiResponse = requests.get(
-		apiQueryURI,
-		headers=apiQueryHeaders
-		)
-	
-	apiResponseData = apiResponse.json()
+	# Connect to the Meraki API.
+	x_cisco_meraki_api_key = Api.query.get_or_404(id).key
+	client = MerakiSdkClient(x_cisco_meraki_api_key)
 
-	outputExcel = pandas.read_json(apiResponse.content).to_excel(outputFilePath + 'Customer Inventory.xlsx')
+	# An instance of the OrganizationsController class can be accessed from the API Client.
+	organizations_controller = client.organizations
+
+	# get_organization_inventory() accepts the organization id as integer input and provides a dictionary as output. Contrary to documentation, get_organization_inventory() does not accept a dictionary as input. 
+	#collect = {}
+	#collect['organization_id'] = organization_id
+
+	try:
+		inventory = organizations_controller.get_organization_inventory(organization_id)
+
+		outputExcel = pandas.read_json(jsonpickle.encode(inventory)).to_excel(outputFilePath + 'Customer Inventory.xlsx')
+
+		return render_template('inventory.html', organization = organization_id, inventory = inventory)
+
+	except Exception as e:
+		return(e)
 	
-	return render_template('inventory.html', apiResponseData = apiResponseData)
+	
 
 # View Networks
-@app.route('/<int:id>/networks')
-def networks(id):
-	organization = OrganizationList.query.get_or_404(id)
-	apiQueryHeaders = {
-		'X-Cisco-Meraki-API-Key': organization.apiKey,
-		'Accept': '*/*',
-		'Accept-Encoding': 'gzip, deflate, br',
-		'Connection': 'keep-alive'
-	}
+@app.route('/key/<int:id>/organization/<organization_id>/networks')
+def networks(id, organization_id):
+	# Connect to the Meraki API.
+	x_cisco_meraki_api_key = Api.query.get_or_404(id).key
+	client = MerakiSdkClient(x_cisco_meraki_api_key)
 
-	apiQueryURI = baseUrl + "/organizations/" + str(id) + "/networks"
-	
-	apiResponse = requests.get(
-		apiQueryURI,
-		headers=apiQueryHeaders
-		)
-	
-	apiResponseData = apiResponse.json()
+	# An instance of the NetworksController class can be accessed from the API Client.
+	networks_controller = client.networks
 
-	outputExcel = pandas.read_json(apiResponse.content).to_excel(outputFilePath + 'Customer Networks.xlsx')
+	# get_organization_networks() accepts a dictionary as input and provides all networks for that organization
+	collect = {}
+	collect['organization_id'] = organization_id
 	
-	return render_template('networks.html', apiResponseData = apiResponseData)
+	try:
+		networks = networks_controller.get_organization_networks(collect)
+
+		outputExcel = pandas.read_json(jsonpickle.encode(networks)).to_excel(outputFilePath + 'Customer Networks.xlsx')
+
+		return render_template('networks.html', organization = organization_id, networks = networks)
+
+	except Exception as e:
+		return(e)
 
 
 if __name__ == "__main__":
